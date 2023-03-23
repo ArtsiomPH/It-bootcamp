d2cc8d6c3415f1914c577d15fbbde9d0e72b0f7d
Artsiom_PH <workartpharm@gmail.com>
2023-03-23 11:17:20 +0300

    add models

--- booksandauthors/models/author.py
+++ booksandauthors/models/author.py
@@ -2,4 +2,16 @@ from django.db import models
 
 
 class Author(models.Model):
-    pass
+    first_name = models.CharField(max_length=55, verbose_name="Имя")
+    second_name = models.CharField(max_length=55, verbose_name="Фамилия")
+    third_name = models.CharField(max_length=55, verbose_name="Отчество", blank=True, null=True)
+    date_of_birth = models.DateField(blank=True, null=True, verbose_name="Дата рождения")
+    biography = models.TextField(max_length=1000, verbose_name="Краткая биография", blank=True, null=True)
+
+    class Meta:
+        ordering = ["second_name", "first_name"]
+
+    def __str__(self):
+        if self.third_name:
+            return f"{self.second_name} " + f"{self.first_name}"[0] + '.' + f'{self.third_name}'[0] + '.'
+        return f"{self.second_name} " + f"{self.first_name}"[0] + '.'
--- booksandauthors/models/book.py
+++ booksandauthors/models/book.py
@@ -1,5 +1,17 @@
 from django.db import models
 
+from ..models import Author
+
 
 class Book(models.Model):
-    pass
+    title = models.CharField(max_length=55, verbose_name="Название")
+    genre = models.CharField(max_length=25, blank=True, null=True, verbose_name="Жанр")
+    pub_date = models.DateField(blank=True, null=True, verbose_name="Дата издания")
+    description = models.TextField(max_length=1000, blank=True, null=True, verbose_name="Краткое описание")
+    author = models.ManyToManyField(Author, verbose_name="Автор")
+
+    class Meta:
+        ordering = ["title"]
+
+    def __str__(self):
+        return self.title
