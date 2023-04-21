4c1c95790d7ff038875fc30246a6466578d4d3d4
Artsiom_PH <workartpharm@gmail.com>
2023-04-21 08:02:39 +0300

    add random books and authors

--- booksandauthors/management/commands/init_admin.py
+++ booksandauthors/management/commands/init_admin.py
@@ -3,15 +3,14 @@ from django.contrib.auth.models import User
 
 
 class Command(BaseCommand):
-
     def handle(self, *args, **options):
         if User.objects.count() == 0:
-            username = 'admin'
-            password = 'admin'
-            print(f'Creating account for {username}')
+            username = "admin"
+            password = "admin"
+            print(f"Creating account for {username}")
             admin = User.objects.create_superuser(username=username, password=password)
             admin.is_active = True
             admin.is_admin = True
             admin.save()
         else:
-            print('Admin accounts can only be initialized if no Accounts exist')
+            print("Admin accounts can only be initialized if no Accounts exist")
--- booksandauthors/management/commands/make_base.py
+++ booksandauthors/management/commands/make_base.py
@@ -4,49 +4,27 @@ from django.core.management.base import BaseCommand
 
 from booksandauthors.models import Author, Book
 
-AUTHORS = [
-    {
-        'first_name': 'Александр',
-        'second_name': 'Пушкин'
-    },
-{
-        'first_name': 'Лев',
-        'second_name': 'Толстой'
-    },
-{
-        'first_name': 'Сергей',
-        'second_name': 'Есенин'
-    },
-
-]
-
-BOOKS = [
-    {
-        'title': 'Евгений Онегин'
-    },
-{
-        'title': 'Анна Каренина'
-    },
-{
-        'title': 'Сборник стихов'
-    },
-
-]
-
 
 class Command(BaseCommand):
     help = "Add some authors and books"
 
     def handle(self, *args, **options):
-        authors_list = []
-        for author in AUTHORS:
-            author_object, _ = Author.objects.get_or_create(**author)
-            authors_list.append(author_object)
+        if Author.objects.count() == 0:
+            authors_list = []
+            for counter in range(20):
+                first_name = f"Имя {counter}"
+                second_name = f"Фамилия {counter}"
+                author_object, _ = Author.objects.get_or_create(
+                    first_name=first_name, second_name=second_name
+                )
+                authors_list.append(author_object)
 
-        for book in BOOKS:
-            book_object, _ = Book.objects.get_or_create(**book)
-            book_object.author.set([random.choice(authors_list)])
+            for counter in range(len(authors_list)):
+                title = f"Название {counter}"
+                book_object, _ = Book.objects.get_or_create(title=title)
+                book_object.author.set([random.choice(authors_list)])
 
+            print("Test base was filled")
 
-
-
+        else:
+            print("Test base is not already empty")
